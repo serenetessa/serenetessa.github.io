5d4e40e3bbef4db59b1078a2a725889794e58e2f
Ilia Lesik <ilialesik@gmail.com>
2016-12-08 11:40:35 +0300

    travis

--- deploy-ghpages.sh
+++ deploy-ghpages.sh
@@ -5,6 +5,8 @@ gulp
  git config user.name "Travis-CI"
  git config user.email "travis@nodemeatspace.com"
  git add .
+ echo "commit"
  git commit -m "Deployed to Github Pages"
+ echo "push https://${GH_TOKEN}@${GH_REF}"
  git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
 )
